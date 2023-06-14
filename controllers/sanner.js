3799c58d74d93b3a9acd217cbbabaa6eacff446a
Isaac  Tingo <newtone480@gmail.com>
2023-06-14 18:03:33 +0300

    Update query according to db structure

--- controllers/sanner.js
+++ controllers/sanner.js
@@ -36,89 +36,106 @@ exports.initiateScanner = async (req, res) => {
             let checkComplete = false;
             let storedSerialNumber = null;
 
-            if (!checkComplete && !storedSerialNumber) {
-              //keep checking
+            if (response) {
+              if (!checkComplete && !storedSerialNumber) {
+                //keep checking
 
-              const interval = setInterval(() => {
-                console.log("---------Checking---------");
-                connection.query(
-                  "SELECT serial_number FROM temp_serial_numbers WHERE soldier_id = ?",
-                  [soldierID],
-                  (err, results) => {
-                    if (results?.length > 0) {
-                      checkComplete = true;
-                      storedSerialNumber = results[0].serial_number;
+                const interval = setInterval(() => {
+                  console.log("---------Checking---------");
+                  connection.query(
+                    "SELECT serial_number FROM temp_serial_numbers WHERE soldier_id = ?",
+                    [soldierID],
+                    (err, results) => {
+                      if (results?.length > 0) {
+                        checkComplete = true;
+                        storedSerialNumber = results[0].serial_number;
+                        storedSerialNumber.replace(/\s/g, "").toLowerCase();
 
-                      clearInterval(interval);
-                      clearTimeout(timeout);
+                        clearInterval(interval);
+                        clearTimeout(timeout);
 
-                      //check user details
-                      connection.query(
-                        "SELECT * FROM devices INNER JOIN users ON devices.user_id = users.id WHERE devices.serial_number = ?",
-                        [storedSerialNumber],
-                        (err, results) => {
-                          if (err) {
-                            console.error("Error executing query: ", err);
-                            res.json({
-                              status: "Failed",
-                              message:
-                                "An error occurred while getting user data associated with the serial number",
-                              error: err,
-                            });
-                          } else {
-                            res.json({
-                              status: "Success",
-                              message: "User data retrieved successfully",
-                              data: results,
-                            });
+                        //check user details
+                        connection.query(
+                          "SELECT * FROM asset_management INNER JOIN staff ON asset_management.staff_id = staff.id INNER JOIN users ON staff.user_id = users.id WHERE REPLACE(asset_management.serial_number, ' ', '') LIKE ?",
+                          [storedSerialNumber.replace(/\s/g, "")],
+                          (err, results) => {
+                            if (err) {
+                              console.error("Error executing query: ", err);
+                              res.json({
+                                status: "Failed",
+                                message:
+                                  "An error occurred while getting user data associated with the serial number",
+                                error: err,
+                              });
+                            } else {
+                              res.json({
+                                status: "Success",
+                                message: "User data retrieved successfully",
+                                data: results,
+                              });
 
-                            //delete the temp_serial record
-                            const query = `DELETE FROM temp_serial_numbers WHERE soldier_id = ?`;
-                            connection.query(
-                              query,
-                              [soldierID],
-                              (err, results) => {
-                                if (err) {
-                                  console.error("Error executing query: ", err);
-                                  res.json({
-                                    status: "Failed",
-                                    message:
-                                      "An error occurred while deleting serial number",
-                                    error: err,
-                                  });
-                                } else {
-                                  console.log(
-                                    "------------Scan completed------------"
-                                  );
+                              // Delete the temp_serial record
+                              const query = `DELETE FROM temp_serial_numbers WHERE soldier_id = ?`;
+                              connection.query(
+                                query,
+                                [soldierID],
+                                (err, results) => {
+                                  if (err) {
+                                    console.error(
+                                      "Error executing query: ",
+                                      err
+                                    );
+                                    res.json({
+                                      status: "Failed",
+                                      message:
+                                        "An error occurred while deleting the serial number",
+                                      error: err,
+                                    });
+                                  } else {
+                                    console.log(
+                                      "------------Scan completed------------"
+                                    );
+                                  }
                                 }
-                              }
-                            );
+                              );
+                            }
                           }
-                        }
-                      );
-                    } else if (err) {
-                      console.error("Error executing query: ", err);
-                      return;
+                        );
+                      } else if (err) {
+                        console.error("Error executing query: ", err);
+                        return;
+                      }
                     }
-                  }
-                );
-              }, 1000);
+                  );
+                }, 1000);
 
-              const timeout = setTimeout(() => {
-                console.log("-----Timeout reached------");
-                res.json({
-                  status: "Failed",
-                  message: "You have not scanned any device",
-                });
-                clearInterval(interval);
-                clearTimeout(timeout);
+                const timeout = setTimeout(() => {
+                  console.log("-----Timeout reached------");
+                  res.json({
+                    status: "Failed",
+                    message: "You have not scanned any device",
+                  });
+                  clearInterval(interval);
+                  clearTimeout(timeout);
 
-                checkComplete = true;
-              }, 300000);
+                  checkComplete = true;
+                }, 300000);
+              }
+            } else {
+              res.json({
+                status: "Failed",
+                message:
+                  "An error occured while trying to send notification to initiate scanner",
+              });
             }
           })
           .catch((error) => {
             console.log("Error sending notification:", error);
+            res.json({
+              status: "Failed",
+              message: "An error occured while trying to send notification",
+              error: error.message,
+            });
           });
       }
     });
@@ -178,6 +195,29 @@ exports.storeSerialNumber = async (req, res) => {
     res.json({
       status: "Failed",
       message: "An error occured while trying to store serialNumber",
+      error: error.message,
+    });
+  }
+};
+
+//get data
+exports.getData = async (req, res) => {
+  let { serialNumber } = req.body;
+  serialNumber = serialNumber.replace(/\s/g, "").toLowerCase(); // Assign the modified value back to the variable
+  try {
+    const getQuery = `SELECT * FROM asset_management WHERE asset_management.serial_number = ?`;
+    connection.query(getQuery, [serialNumber], (err, results) => {
+      if (err) {
+        res.send(err.message);
+      } else {
+        res.send(results);
+      }
+    });
+  } catch (error) {
+    console.log(error);
+    res.json({
+      status: "Failed",
+      message: "An error occurred while trying to get data",
       error: error.message,
     });
   }
--- routes/scanner.js
+++ routes/scanner.js
@@ -4,10 +4,13 @@ const {
   storeSerialNumber,
   getUsers,
   sendNotif,
+  getData,
 } = require("../controllers/sanner");
 const router = express.Router();
 
 router.post("/initiate-scanner", initiateScanner);
 router.post("/store-serial-number", storeSerialNumber);
+
+router.get("/get-data", getData);
 
 module.exports = router;
